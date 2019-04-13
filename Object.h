8b4e2c6fef2a9cf4540c92eac1f20ffa22b79d65
tomotakaeru <tomotakaeru@gmail.com>
2019-04-13 11:48:55 +0900

    8.1正八面体，正六面体を描画

--- Object.h
+++ Object.h
@@ -7,20 +7,25 @@ class Object {
 	GLuint vao;
 	// 頂点バッファオブジェクト名
 	GLuint vbo;
+	// インデックスの頂点バッファオブジェクト
+	GLuint ibo; 
 
 public:
 
 	// 頂点属性
 	struct Vertex {
 		// 位置
-		GLfloat position[2];
+		GLfloat position[3];
 	};
 
 	//コンストラクタ
 	// size: 頂点の位置の次元
 	// vertexcount: 頂点の数
 	// vertex: 頂点属性を格納した配列
-	Object(GLint size, GLsizei vertexcount, const Vertex *vertex)
+	// indexcount: 頂点のインデックスの要素数
+	// index: 頂点のインデックスを格納した配列
+	Object(GLint size, GLsizei vertexcount, const Vertex *vertex,
+		GLsizei indexcount = 0, const GLuint *index = NULL)
 	{
 		// 頂点配列オブジェクト
 		glGenVertexArrays(1, &vao);
@@ -34,6 +39,12 @@ public:
 		// 結合されている頂点バッファオブジェクトを in 変数から参照できるようにする
 		glVertexAttribPointer(0, size, GL_FLOAT, GL_FALSE, 0, 0);
 		glEnableVertexAttribArray(0);
+
+		// インデックスの頂点バッファオブジェクト
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+			indexcount * sizeof (GLuint), index, GL_STATIC_DRAW); 
 	}
 
 	// デストラクタ
@@ -44,6 +55,9 @@ public:
 
 		// 頂点バッファオブジェクトを削除
 		glDeleteBuffers(1, &vbo);
+
+		// インデックスの頂点バッファオブジェクトを削除する
+		glDeleteBuffers(1, &ibo); 
 	}
 
 private:
--- test_text.cpp
+++ test_text.cpp
@@ -6,6 +6,7 @@
 #include "Shape.h"
 #include "Window.h"
 #include "Matrix.h"
+#include "ShapeIndex.h"
 
 
 // Display compile result of shader obj
@@ -162,15 +163,61 @@ static void OnMouseEvent(Window *window, int button,
 }
 */
 
-// 矩形の頂点の位置
+// 矩形の頂点の位置（Object.hの構造体Vertexのメンバpositionを二次元にする必要）
 constexpr Object::Vertex rectangleVertex[] =
 {
-  { -0.5f, -0.5f },
-  {  1.5f, -0.5f },
-  {  1.5f,  1.5f },
-  { -0.5f,  1.5f }
+	{ -0.5f, -0.5f },
+	{  1.5f, -0.5f },
+	{  1.5f,  1.5f },
+	{ -0.5f,  1.5f }
 };
 
+// 八面体の頂点の位置
+constexpr Object::Vertex octahedronVertex[] =
+{
+	{  0.0f,  1.0f,  0.0f },
+	{ -1.0f,  0.0f,  0.0f },
+	{  0.0f, -1.0f,  0.0f },
+	{  1.0f,  0.0f,  0.0f },
+	{  0.0f,  1.0f,  0.0f },
+	{  0.0f,  0.0f,  1.0f },
+	{  0.0f, -1.0f,  0.0f },
+	{  0.0f,  0.0f, -1.0f },
+	{ -1.0f,  0.0f,  0.0f },
+	{  0.0f,  0.0f,  1.0f },
+	{  1.0f,  0.0f,  0.0f },
+	{  0.0f,  0.0f, -1.0f }
+}; 
+
+// 六面体の頂点の位置
+constexpr Object::Vertex cubeVertex[] =
+{
+	{ -1.0f, -1.0f, -1.0f },  // (0)
+	{ -1.0f, -1.0f,  1.0f },  // (1)
+	{ -1.0f,  1.0f,  1.0f },  // (2)
+	{ -1.0f,  1.0f, -1.0f },  // (3)
+	{  1.0f,  1.0f, -1.0f },  // (4)
+	{  1.0f, -1.0f, -1.0f },  // (5)
+	{  1.0f, -1.0f,  1.0f },  // (6)
+	{  1.0f,  1.0f,  1.0f }   // (7)
+}; 
+
+// 六面体の稜線の両端点のインデックス
+constexpr GLuint wireCubeIndex[] =
+{
+	1, 0, // (a)
+	2, 7, // (b)
+	3, 0, // (c)
+	4, 7, // (d)
+	5, 0, // (e)
+	6, 7, // (f)
+	1, 2, // (g)
+	2, 3, // (h)
+	3, 4, // (i)
+	4, 5, // (j)
+	5, 6, // (k)
+	6, 1  // (l)
+}; 
 
 int test_text(void)
 {
@@ -204,8 +251,11 @@ int test_text(void)
 	const GLint projectionLoc(glGetUniformLocation(program, "projection"));
 
 	// Create shape data
-	std::unique_ptr<const Shape> shape(new Shape(2, 4, rectangleVertex)); 
-	
+	//std::unique_ptr<const Shape> shape(new Shape(2, 4, rectangleVertex)); 
+	//std::unique_ptr<const Shape> shape(new Shape(3, 12, octahedronVertex)); 
+	std::unique_ptr<const Shape> shape(new ShapeIndex(3, 8, cubeVertex,
+		24, wireCubeIndex));
+
 	// Loop until the user closes the window
 	while (!window.shouldClose()) {
 		// Clear window
