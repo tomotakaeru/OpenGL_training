a6b342a5c39571a86393c68f261c360e22c9be09
DESKTOP-1J7U2I0\tomot <tomotakaeru@gmail.com>
2019-04-16 12:10:41 +0900

    .pngを読み取るライブラリやヘッダファイルや関数を追加（mainでの動作は未確認）

--- homography_transport/cpp/main.cpp
+++ homography_transport/cpp/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <vector>
+#include <libpng/png.h>
 //#include <Eigen/Core> //not in use, path check
 
 
@@ -17,6 +18,91 @@ static void OnMouseEvent(GLFWwindow *window, int button,
 	if (button == GLFW_MOUSE_BUTTON_LEFT) {
 		printf("Mouse: %f %f\n", x, y);
 	}
+}
+	
+GLuint createTextureFromPNGFile(const char *filename) {
+
+	FILE *fp;
+	png_structp pPng = NULL;
+	png_infop pInfo = NULL;
+	int depth, colorType, interlaceType;
+	unsigned int width, height;
+	int rowSize, imgSize;
+	unsigned int i;
+	unsigned char *data;
+	GLuint texture;
+
+	//PNGファイルを開く
+	fopen_s(&fp, filename, "rb");
+	if (!fp) {
+		fprintf(stderr, "createTextureFromPNGFile: Failed to fopen.");
+		return 0;
+	}
+
+	//PNGファイルを読み込むための構造体を作成
+	pPng = png_create_read_struct(
+		PNG_LIBPNG_VER_STRING,
+		NULL, NULL, NULL
+	);
+	pInfo = png_create_info_struct(pPng);
+
+	//初期化
+	png_init_io(pPng, fp);
+
+	//画像情報を読み込み
+	//画像の幅、高さ、ビット深度、色の表現方法、インターレースの情報を取得する
+	png_read_info(pPng, pInfo);
+	png_get_IHDR(pPng, pInfo,
+		&width, &height,
+		&depth, &colorType,
+		&interlaceType, NULL, NULL
+	);
+
+	//RGBとRGBAのみに対応
+	if (colorType != PNG_COLOR_TYPE_RGB && colorType != PNG_COLOR_TYPE_RGBA) {
+		fprintf(stderr, "createTextureFromPNGFile: Supprted color type are RGB and RGBA.");
+		return 0;
+	}
+
+	//インターレースは非対応
+	if (interlaceType != PNG_INTERLACE_NONE) {
+		fprintf(stderr, "createTextureFromPNGFile: Interlace image is not supprted.");
+		return 0;
+	}
+
+	//1行のデータサイズと画像の高さから必要なメモリ量を計算して、メモリ確保
+	rowSize = png_get_rowbytes(pPng, pInfo);
+	imgSize = rowSize * height;
+	data = (unsigned char*)malloc(imgSize); //void*の逆参照はキャストする必要
+
+	//ピクセルの読み込み
+	for (i = 0; i < height; i++) {
+		png_read_row(pPng, &data[i * rowSize], NULL);
+	}
+
+	png_read_end(pPng, pInfo);
+
+	//OpenGLテクスチャの作成
+	glGenTextures(1, &texture);
+
+	//テクスチャを選択
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	//テクスチャにPNGファイルから読み込んだピクセルを書き込む
+	glTexImage2D(
+		GL_TEXTURE_2D, 0, GL_RGBA,
+		width, height,
+		0, GL_RGBA, GL_UNSIGNED_BYTE, data
+	);
+
+	//後片付け
+	free(data);
+	png_destroy_info_struct(pPng, &pInfo);
+	png_destroy_read_struct(&pPng, NULL, NULL);
+	fclose(fp);
+
+	return texture;
+
 }
 
 
